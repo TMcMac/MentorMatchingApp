9aabed14f96ca8e84071b403be7fe659440d632a
Jilroge7 <1672@holbertonschool.com>
2022-01-25 03:45:46 +0000

    fixing what I broke because I am a dummy

--- collab-app/src/components/Scream.js
+++ collab-app/src/components/Scream.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import { withStyles } from '@mui/styles';
 import Typography from '@mui/material/Typography';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import PropTypes from 'prop-types';
+//Redux stuff
+//import { connect } from 'react-redux';
 
 //MUI stuff
 import Card from '@mui/material/Card';
@@ -10,28 +15,40 @@ import CardMedia from '@mui/material/CardMedia';
 
 const styles = {
   card: {
-    display: 'flex'
+    position: 'relative',
+    display: 'flex',
+    marginBottom: 20
+  },
+  image: {
+    minWidth: 200
+  },
+  content: {
+    padding: 25,
+    objectFit: 'cover'
   }
-}
+};
 
 class Scream extends Component {
   render() {
+      dayjs.extend(relativeTime);
       const { classes, scream : { body, createdAt, userImage, userHandle, screamId, likeCount, commentCount } } = this.props
     return (
-      <Card>
+      <Card className={classes.card}>
         <CardMedia
         image={userImage}
-        title="Profile image"/>
-        <CardContent>
+        title="Profile image" className={classes.image}/>
+        <CardContent className={classes.content}>
           <Typography 
             variant="h5"
             component={Link}
-            to={/users/${userHandle}`}
+            to={`/users/${userHandle}`}
             color='primary'
           >
             {userHandle}
           </Typography>
-          <Typography variant="body2" color="textSecondary">{createdAt}</Typography>
+          <Typography variant="body2" color="textSecondary">
+            {dayjs(createdAt).fromNow()}
+          </Typography>
           <Typography variant="body1">{body}</Typography>
         </CardContent>
       </Card>
---
+++ collab-app/src/firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseApp = {
+  apiKey: "AIzaSyBxu08rqeCJAXW-cwj91jsIO3B16y6GJ9g",
+  authDomain: "lxai-mentor-matching.firebaseapp.com",
+  projectId: "lxai-mentor-matching",
+  storageBucket: "lxai-mentor-matching.appspot.com",
+  messagingSenderId: "943361642853",
+  appId: "1:943361642853:web:ea8a3c18cd857ce164d18c",
+  measurementId: "G-4E5GEQJQTX"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export default app;
